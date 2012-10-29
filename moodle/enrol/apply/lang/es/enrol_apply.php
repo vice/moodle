a1aee6c733907204635bf2769a57a6621a93e84f
Vicente Jimenez Aguilar <googuy@gmail.com>
2012-10-29 22:44:50 +0100

    Fisrt Spanish translation attempt

---
+++ moodle/enrol/apply/lang/es/enrol_apply.php
@@ -0,0 +1,39 @@
+<?php
+ 
+// The name of your plugin. Displayed on admin menus.
+$string['enrolname'] = 'Solicitar matrícula';
+$string['pluginname'] = 'Solicitar matrícula';
+
+$string['confirmmailsubject'] = 'Asunto del correo de confirmación';
+$string['confirmmailcontent'] = 'Contenido del correo de confirmación';
+$string['cancelmailsubject'] = 'Asunto del correo de cancelación';
+$string['cancelmailcontent'] = 'Contenido del correo de cancelación';
+$string['mailaddress'] = 'Send mail address'; // I need to clarify this using the plugin
+$string['mailusername'] = 'Send mail username'; // I need to clarify this using the plugin
+$string['mailpassword'] = 'Send mail password'; // I need to clarify this using the plugin
+$string['confirmmailcontent_desc'] = 'Por favor, use marcas especiales que se substituirán en el contenido del correo.<br>{firstname}:Nombre registrado por el usuario; {content}:Nombre del curso';
+$string['cancelmailcontent_desc'] = 'Por favor, use marcas especiales que se substituirán en el contenido del correo.<br>{firstname}:Nombre registrado por el usuario; {content}:Nombre del curso';
+
+$string['confirmusers'] = 'Confirmar matrícula';
+
+$string['coursename'] = 'Curso';
+$string['applyuser'] = 'Nombre / Apellido';
+$string['applyusermail'] = 'Correo electrónico';
+$string['applydate'] = 'Fecha de solicitud'; // Enrol date -> Fecha de matriculación
+$string['btnconfirm'] = 'Confirmar';
+$string['btncancel'] = 'Cancelar';
+$string['enrolusers'] = 'Matricular usuarios';
+
+$string['status'] = 'Aceptar matriculación tras aprovación';
+$string['confirmenrol'] = 'Gestionar solicitud';
+
+$string['apply:config'] = 'Configurar instancias de solicitar matrícula';
+$string['apply:manage'] = 'Gestionar solicitar matrícula';
+$string['apply:unenrol'] = 'Cancelar usuarios del curso';
+$string['apply:unenrolapply'] = 'Cancelarse a si mismo del curso';
+ 
+// Description of your plugin. Shown on the plugin's configuration screen.
+$string['description'] = '';
+$string['notification'] = '<b>Solicitud de matriculación enviada correctamente</b>. <br/><br/>Será notificado por correo electrónico en cuanto se confirme su matriculación. Si desea matricularse en otros cursos, por favor, seleccione "catálogo de cursos" del menú principal.';
+ 
+?>
